85444a243c7e35f8243730be114152e5be36a810
girija05-giri <girijamadithapu@gmail.com>
2026-06-14 18:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+st.set_page_config(
+    page_title="Product Recommendation System",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
+st.title("AI Product Recommendation System")
+st.write("Use the sidebar to navigate between pages.")
 
-st.set_page_config(page_title="Product Recommender", layout="wide")
 
 st.sidebar.title("Navigation")
 
